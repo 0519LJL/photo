0c92ab0a99c553c4271aefc3d73897cf7a04356c
LJL <781867500@qq.com>
2019-01-02 18:32:57 +0800

    修改图片展示

--- MyPhotoManagement/Controllers/PhotoController.cs
+++ MyPhotoManagement/Controllers/PhotoController.cs
@@ -24,18 +24,18 @@ namespace MyPhotoManagement.Controllers
             string[] base64strs = base64str.Split(',');
             try
             {
-                for (int i = 0; i < base64strs.Length; i++)
-                {
-                    byte[] bt = Convert.FromBase64String(base64strs[i]);//获取图片base64
-                    string fileName = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString();//年月
-                    string ImageFilePath = "/Image" + "/" + fileName;
-                    if (System.IO.Directory.Exists(HttpContext.Current.Server.MapPath(ImageFilePath)) == false)//如果不存在就创建文件夹
-                    {
-                        System.IO.Directory.CreateDirectory(HttpContext.Server.MapPath(ImageFilePath));
-                    }
-                    string ImagePath = HttpContext.Current.Server.MapPath(ImageFilePath) + "/" + System.DateTime.Now.ToString("yyyyHHddHHmmss");//定义图片名称
-                    File.WriteAllBytes(ImagePath + ".png", bt); //保存图片到服务器，然后获取路径  
-                }
+                //for (int i = 0; i < base64strs.Length; i++)
+                //{
+                //    byte[] bt = Convert.FromBase64String(base64strs[i]);//获取图片base64
+                //    string fileName = DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString();//年月
+                //    string ImageFilePath = "/Image" + "/" + fileName;
+                //    if (System.IO.Directory.Exists(HttpContext.Current.Server.MapPath(ImageFilePath)) == false)//如果不存在就创建文件夹
+                //    {
+                //        System.IO.Directory.CreateDirectory(HttpContext.Server.MapPath(ImageFilePath));
+                //    }
+                //    string ImagePath = HttpContext.Current.Server.MapPath(ImageFilePath) + "/" + System.DateTime.Now.ToString("yyyyHHddHHmmss");//定义图片名称
+                //    File.WriteAllBytes(ImagePath + ".png", bt); //保存图片到服务器，然后获取路径  
+                //}
                 
             }
             catch (Exception e)
